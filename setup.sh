95a31a3b55a42ffa8a7ed992f11e774e8823ee8a
Stephan Raj <124070824+stephanrpicloud@users.noreply.github.com>
2024-08-11 19:31:52 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p ~/.streamlit/
-echo "[server]\nheadless = true\nport = $PORT\nenableCORS = false\n\n" > ~/.streamlit/config.toml
-pip install git+https://github.com/victoryhb/streamlit-option-menu.git
